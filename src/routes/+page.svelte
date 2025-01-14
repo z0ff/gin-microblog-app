c82773b6997503af94d5b8aaf2675ab8d7ab68ce
z0ff <39230877+z0ff@users.noreply.github.com>
2025-01-15 01:18:16 +0900

    投稿表示の試作

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,85 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let response: Promise<any>;
+
+	let postContent: HTMLTextAreaElement;
+	let userId = 1;
+
+	let postButtonEnabled = false;
 
 	onMount(() => {
-		response = (async () => {
-			const res = await fetch('http://localhost:3000/', {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			}).catch(console.error);
-
-			// return res.json();
-
-			if (res) {
-				return res.json();
-			} else {
-				return { error: res.statusText };
-			}
-		})();
-
-		response.then(console.log);
+		postContent = document.getElementById('post-content') as HTMLTextAreaElement;
 	});
+
+	const getPosts = async () => {
+		const responseJson = await getPostsJson();
+		if (responseJson.error) {
+			return { error: responseJson.error };
+		}
+
+		const posts = JSON.parse(responseJson);
+		// console.log(posts);
+
+		return posts;
+	};
+
+	const getPostsJson = async () => {
+		const res = await fetch('http://localhost:3000/', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).catch(console.error);
+
+		if (res) {
+			return await res.json()
+		} else {
+			return { error: res.statusText };
+		}
+	};
+	let posts = getPostsJson();
+	console.log(posts);
+
+	const submitPost = async () => {
+		const res = await fetch('http://localhost:3000/post', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ user_id: userId, content: postContent.value }),
+		}).catch(console.error);
+
+		if (res) {
+			console.log(await res.json());
+		} else {
+			console.error(res);
+		}
+
+		posts = await getPostsJson();
+	};
+
+	const toggleEnabledPostButton = () => {
+		postButtonEnabled = postContent.value.length > 0;
+	};
 
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-{#await response}
+<h1>microblog</h1>
+
+<textarea on:input={toggleEnabledPostButton} rows="10" cols="50" placeholder="type something here" id="post-content"></textarea>
+<button on:click={submitPost} type="button" id="post-submit" disabled={!postButtonEnabled}>submit</button>
+
+{#await posts}
 	<p>loading...</p>
 {:then data}
-	<pre>{JSON.stringify(data, null, 2)}</pre>
+	{#each data as post}
+		<div>
+			<p>by {post.User.DisplayName}</p>
+			<p>{post.CreatedAt}</p>
+			<p>{post.Content}</p>
+		</div>
+	{/each}
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,6 @@
+<h1>login</h1>
+<form action="http://localhost:3000" method="post">
+    <input type="text" name="username" placeholder="username" />
+    <input type="password" name="password" placeholder="password" />
+    <button type="submit">login</button>
+</form>
