5375d5c5211516368b6a235fcecc458f22de06dd
z0ff <39230877+z0ff@users.noreply.github.com>
2025-02-18 05:19:22 +0900

    like(いいね)機能の実装

--- src/lib/Components/Post.svelte
+++ src/lib/Components/Post.svelte
@@ -1,11 +1,52 @@
 <script>
     import { strToDate } from "$lib/datetime";
     export let post;
+    const postID = post.ID;
+    let isLiked = post.IsLiked;
+
+    const onLikeBtnClicked = async () => {
+        const res = await fetch(`http://localhost:3000/like/${postID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // fetch APIはデフォルトでセッション情報を送信しないため、
+            // 明示的に送信するように設定する
+            credentials: "include"
+        }).catch(console.error);
+
+        if (res) {
+            console.log(await res.json());
+            isLiked = true;
+        } else {
+            console.error(res);
+        }
+    }
+
+    const onUnLikeBtnClicked = async () => {
+        const res = await fetch(`http://localhost:3000/unlike/${postID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // fetch APIはデフォルトでセッション情報を送信しないため、
+            // 明示的に送信するように設定する
+            credentials: "include"
+        }).catch(console.error);
+
+        if (res) {
+            console.log(await res.json());
+            isLiked = false;
+        } else {
+            console.error(res);
+        }
+    }
 </script>
 <div class="card card-bordered bg-base-50 shadow-lg">
-                <div class="card-body">
-                    <a href="/@{post.User.Name}"><span class="font-bold">{post.User.DisplayName}</span> @{post.User.Name}</a>
-                    <p>{strToDate(post.CreatedAt)}</p>
-                    <p class="whitespace-pre-wrap">{post.Content}</p>
-                </div>
-            </div>
+    <div class="card-body">
+        <a href="/@{post.User.Name}"><span class="font-bold">{post.User.DisplayName}</span> @{post.User.Name}</a>
+        <p>{strToDate(post.CreatedAt)}</p>
+        <p class="whitespace-pre-wrap">{post.Content}</p>
+        <button class="btn {isLiked ? 'btn-primary' : 'btn-ghost'}" on:click={isLiked ? onUnLikeBtnClicked : onLikeBtnClicked}>{isLiked ? "Unlike" : "Like"}</button>
+    </div>
+</div>
